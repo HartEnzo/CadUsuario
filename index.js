a2d0467e998ee1e9029fc715fe63aec96972b0f5
HartEnzo <enzoguihartmann@gmail.com>
2024-07-30 10:41:42 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,81 +1,101 @@
 const prompt = require('prompt-sync')()
 const usuarios = []
 
-let ultimoID = 1
+let ultimoID = 0;
 
-const emailValido = email => {
-      let valido = true
-    
-      usuarios.forEach(usuario => {
-        if(email == usuario.email){
-            console.log("Email inválido")
-            valido = false
-        }
-      })
-    
-      return valido && email != "" && email.includes("@")
-}
-const modelo = () => {
-    const nome = prompt("Digite seu nome: ")
-    const email = prompt("Digite o seu email: ")
+const emailNDuplicado = (email, ID) => {
+    if(usuarios.find(usuario => usuario.email == email && usuario.ID != ID)) {
+        console.log("Email duplicado! ")
+        return false
+    }
+    return true
+
+};
+const modelo = (ID = ++ultimoID) => {
+    const nome = prompt("Digite seu nome: ");
+    const email = prompt("Digite o seu e-mail: ");
     const telefones = []
 
     while(true) {
-        const telefone = prompt = prompt("Digite o número do usuário, ou digite 'sair': ")
+        const telefone = prompt("Digite o número de telefone, ou 0 para sair: ");
 
-        if(telefone == 'sair') {
-            break
+        if(telefone == 0) {
+            break;
         }
-    telefones.push(telefone)
+    telefones.push(telefone);
     }
-        if(nome != "" && telefones.length > 0 && emailValido(email)){
-            
-            if(ID != undefined) {
-            const usuario = {
+        if(emailNDuplicado(email, ID)) {
+            return{
                 nome, 
                 email, 
                 telefones, 
                 ID,
             };
-        } else{
-             usuario = {
-                nome, 
-                email, 
-                telefones, 
-                ID: ++ultimoID
+        }
+        
+    console.log("Falha em realizar o procedimento");
         };
-        ultimoID++;
-    return usuario
-    }
-    console.log("Os dados inseridos são inválidos! ")
-};
 
-const criar = () => {
-    const usuario = modelo 
+    const criar = () => {
+         const novo = modelo(); 
     
-    if(usuario != undefined) {
-        usuarios.push(usuario)
+    if(novo) {
+        usuarios.push(novo);
+        console.log("Usuário criado com sucesso! ")
     }
 };
-
-const ler = () =>{
+const listar = () =>{
+   if(usuarios.length == 0) {
+    console.log("Lista de usuários vazia");
+    return false;
+   }else {
     usuarios.forEach(usuario => {
         console.log(`
         ID: ${usuario.ID},
         Email: ${usuario.nome},
-        Telefone: ${usuario.telefone} `)
-    })
-}
-const atualizar = () => {
-    ler();
-    const ID = prompt("Digite o ID que deseja atualizar: ");
-
-    const novo = modelo(ID);
-
-    usuarios.forEach((usuario, indice) => {
-        if(ID == usuario.ID)
-    })
+        Telefone: ${usuario.telefone} `);
+    usuario.telefones.forEach((telefone, indice) => {
+        console.log(`Telefone ${indice + 1}: ${telefone}`);
+    });
+});
+return true;
+  }
 };
 
+const atualizar = () => {
+    if(listar()) {
+        const ID = prompt("Digite o ID que deseja atualizar: ");
+    
+        const novo = modelo(ID);
 
+        const indice = usuarios.findIndex(usuario => ID == usuario.ID)
 
+        if(indice != -1) {
+            usuarios[indice] = novo
+      
+        }else {
+            console.log('ID inexistente')
+        }
+    }
+};
+
+const remover = () => {
+    if(listar()) {
+        const ID = prompt("Qual o ID do usuário que deseja remover? ")
+
+        const indice = usuarios.findIndex(usuario => ID == usuario.ID)
+
+        if(indice != -1) {
+            usuarios.splice(indice, 1)
+        }else {
+            console.log('ID inexistente')
+        }
+    }
+}
+
+module.exports = {
+    criar,
+    atualizar,
+    remover,
+    listar
+}
